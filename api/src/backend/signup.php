91c9d561cb5f935c9f9b7a0fb4ba6bd6e3890694
samuel <santiago.samuelpch2205@gmail.com>
2024-10-10 12:03:59 -0500

    verificacion de correo

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -1,23 +1,30 @@
 <?php
- 
+ //DB cpnnetion
 require('../../config/db_connection.php');
-
+// Get data from register form
    $email = $_POST['email'];
    $password = $_POST['passwd'];
+   //encrypt password with md5 hashing algorithm
    $enc_pass = md5($password);
+   //Query to inseet data into users table
+   $query = "SELECT * FROM users WHERE email = '$email'";
+   $result = pg_query($conn,$query);
+   $row = pg_fetch_assoc($result);
+   if ($row) {
+      echo "<script>alert('email already exists!')</script>";
+      header ('refresh:0;   url=http://127.0.0.1/beta/api/src/register_form.html');
+      exit();
+      }
 
-   //echo "correo_electronico: " . $email;
-   //echo "<br>contraseña: " . $password;
-   //echo "<br> enc. password: " . $enc_pass;
-
-
-   $query = "INSERT INTO users (email, password)
+   $query = "INSERT INTO users (email, password)   
        VALUES ('$email','$enc_pass');
    ";
-
+//Execute query
   $result = pg_query ($conn, $query);
+
    if ($result) {
-      echo "registration succesful!";
+      echo "<script>alert('registation successful')</script>";
+      header ('refresh:0;   url=http://127.0.0.1/beta/api/src/login_form.html');
    } else {
       echo "registration failed!";
    }
